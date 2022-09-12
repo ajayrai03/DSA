76ea65fd82ee577f92ee5084293eab4c92275bee
ajayrai03 <ajayrai1012@gmail.com>
2022-09-12 21:59:58 +0530

    java installed

--- Main.java
+++ Main.java
@@ -1,16 +1,18 @@
-import java.util.*;
+import java.util.Scanner;
 
-public class Main{
-    public static void main(String[] args){
-Scanner scn=new Scanner(System.in);
-int n=scn.nextInt();
-int[]a =new int[n];
-for(int i=0;i<a.length;i++){
-    a[i]=scn.nextInt();
-}
-
-for(int i=0;i<a.length;i++){
-    System.out.println(a[i]);
-}
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("enter your input:");
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = scn.nextInt();
+        }
+        System.out.println("array is :");
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+        scn.close();
     }
 }
