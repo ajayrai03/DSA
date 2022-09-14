551f2b0092c9dcff11112ccf9fc47be4e882998c
ajayrai03 <ajayrai1012@gmail.com>
2022-09-14 21:59:58 +0530

    generic tree

---
+++ normal_queue.java
@@ -0,0 +1,3 @@
+public class normal_queue {
+    
+}
---
+++ queue.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class queue{
+public static void main(String[] args){
+    Queue<Integer> que=new ArrayDeque<>();
+    que.add(20);
+    System.out.println(que);
+    que.add(30);
+    System.out.println(que);
+    que.remove();
+    System.out.println(que);
+    que.add(40);
+    System.out.println(que.peek());
+    System.out.println(que);
+}
+}
