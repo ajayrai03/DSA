ab2c8652c260c111ccec94d7f8aa72eef91b778e
ajayrai03 <ajayrai1012@gmail.com>
2022-10-08 00:57:19 +0530

    dsa1

--- Stacks_and_Queues/sliding_window.java
+++ Stacks_and_Queues/sliding_window.java
@@ -1,7 +1,8 @@
-import java.io.*;
+// import java.io.*;
+package Stacks_and_Queues;
 import java.util.*;
 
-public class Main{
+public class sliding_window{
   
 
 public static void main(String[] args) throws Exception {
@@ -41,14 +42,15 @@ public static void main(String[] args) throws Exception {
         }
         System.out.println(a[j]);
     }
-   
+   scn.close();
  }
 }
----------------------------------------------------------------------------
-1. You are given a number n, representing the size of array a.
+
+/*1. You are given a number n, representing the size of array a.
 2. You are given n numbers, representing the elements of array a.
 3. You are given a number k, representing the size of window.
 4. You are required to find and print the maximum element in every window of size k.
 
 e.g.
 for the array [2 9 3 8 1 7 12 6 14 4 32 0 7 19 8 12 6] and k = 4, the answer is [9 9 8 12 12 14 14 32 32 32 32 19 19 19]
+*/
---
+++ binary_tree/bt_construction.java
@@ -0,0 +1,18 @@
+package binary_tree;
+
+public class bt_construction {
+    public static class Node{
+        int data;
+        Node left;
+        Node right;
+        // constructor
+        Node(int data,Node left,Node right){
+            this.data=data;
+            this.left=left;
+            this.right=right;
+        }
+    }
+    public static void main(String[] args){
+
+    }
+}
