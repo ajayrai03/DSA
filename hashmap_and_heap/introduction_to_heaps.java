fbc70db89ff34c8c02a42643c1f6006736709971
ajayrai03 <ajayrai1012@gmail.com>
2022-10-29 20:16:49 +0530

    hashmap

--- hashmap_and_heap/introduction_to_heaps.java
+++ hashmap_and_heap/introduction_to_heaps.java
@@ -6,8 +6,8 @@ public class introduction_to_heaps {
     public static void main(String[] args){
         // Collections.reverseOrder() => gives priority to bigger value, amd without it. it gives priority to smaller value. so smaller value is printed first
         
-        PriorityQueue<Integer> pq=new PriorityQueue<>(Collections.reverseOrder());
-        // PriorityQueue<Integer> pq=new PriorityQueue<>();
+        // PriorityQueue<Integer> pq=new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
         int[] ranks={22,99,3,11,88,4,1};
         for(int val:ranks){
             pq.add(val);   //nlogn
